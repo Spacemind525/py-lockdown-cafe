b3f84c612771a3ccc20a5a625a534521ad6d12d5
Alex <zigzaksasha@gmail.com>
2025-06-22 16:16:12 +0200

    deleted some flake8 errors

--- app/cafe.py
+++ app/cafe.py
@@ -18,7 +18,7 @@ class Cafe:
 
         if visitor.get("vaccine").get("expiration_date") < date_today:
             raise OutdatedVaccineError(
-                f"{visitor.get('name')} with expired vaccine")
+                f"{visitor.get('name')} vaccine is expired")
 
         if not visitor["wearing_a_mask"]:
             raise NotWearingMaskError(
